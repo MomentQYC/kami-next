2362c29fc1ce84595036386b665d0349f7cbb0e0
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2026-03-30 22:41:39 +0800

    fix

--- src/hooks/app/use-music.ts
+++ src/hooks/app/use-music.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useMusicStore } from '~/atoms/music'
 
 export const useMusic = (musicList: number[] | null) => {
@@ -22,5 +22,6 @@ export const useMusic = (musicList: number[] | null) => {
 }
 
 export const useNoteMusic = (music?: string) => {
-  useMusic(music ? [parseInt(music)] : null)
+  const musicList = useMemo(() => (music ? [parseInt(music)] : null), [music])
+  useMusic(musicList)
 }
